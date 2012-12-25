901d0fbb75f8f6e58a020c05971ae2862c4daa1e
Alex Brown <alexbbrown@glimmer.rstudio.com>
2012-12-25 07:28:52 +0000

    works - using this to pass the function (bad style).
    also added function names to functions for ease of debugging.

--- www/d3widget.js
+++ www/d3widget.js
@@ -38,21 +38,22 @@
   // Object/Array/Other.  Treats this as an argument to pass on.
   // relies on caller invoking returned function using:
   // applyObjectArrayOtherF.call(context,a,b,c) to load this properly
-  function applyObjectArrayOtherF(objF, arrayF, otherF) { return function(val) {
+  function applyObjectArrayOtherF(objF, arrayF, otherF) { 
+    return function applyObjectArrayOther(val) {
     // map context is passed in as this
-    return _.isArray(val) ? arrayF(val, this) : 
-           _.isObject(val) ? objF(val, this) :
-            otherF(val, this);
+    return ((_.isArray(val) ? arrayF: 
+           _.isObject(val) ? objF:
+            otherF).call(this, val, this));
   }}
   
   // Apply a function to every property of an object and return
   // the object constructed out of the results with the same keys
   // pass a context object too.
-  function mapObjectF(f) { return function(obj, context) {
+  function mapObjectF(f) { return function mapObject(obj, context) {
       return _.object(_.keys(obj), _.map(_.values(obj), f, context));      
   }}
   
-  function mapArrayF(f) { return function(obj, context) {
+  function mapArrayF(f) { return function mapArray(obj, context) {
     return _.map(obj, f, context);
   }}
   
@@ -68,13 +69,15 @@
   
   // Something like the Y combinator for these functions
   // allows recursion on subobjects.
-  var recurse=function(val,f) {
-    return f(val,f);
+  // there's an issue with _.map since it passes the context
+  // argument via 'this'.  why does mapObjectF work?
+  function recurse(val,f) {
+    return f.call(f,val,f);
   }  
 
   
   // The K combinator
-  function constantly(val) { return function() { return val }}
+  function constantlyF(val) { return function constantly() { return val }}
   
   var mapStruct = function(struct, f, context) {
     
@@ -99,8 +102,11 @@
     var T=function(x){return typeof(x)}
 
     var Q=mapObjectF(applyObjectArrayOtherF(recurse,_.size,identityF()))
-    Q(message,Q)
-    var Q2=mapAnythingF(applyObjectArrayOtherF(recurse,recurse,function(x){return ">"+x}))
+    //Q(message,Q)
+    var Q2=mapAnythingF(applyObjectArrayOtherF(recurse,recurse,function(x){return ">"+x}));
+    var testStr = {a: [1,2,3], b: {a: "alex", b: "bob"}, c: "hello"};
+    Q2.call(testStr,Q2)
+    Q2.call(Q2,message,Q2)
     Q2(message,Q2)
     // Q2 breaks because of the behaviour of THIS and how mapAnythingF relies on
     // applyObjectArrayOtherF and it's behaviour wrt this.  need to example carefully.
