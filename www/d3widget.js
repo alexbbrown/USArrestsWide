323da40407546f445d8f78a156d4b14e0e0919a8
Alex Brown <alexbbrown@glimmer.rstudio.com>
2012-12-25 08:18:36 +0000

    have wrapped up structurewalker d3 style.  now use structurewalker in decodeData.

--- www/d3widget.js
+++ www/d3widget.js
@@ -15,77 +15,101 @@
         console.log("bad message format");
         return undefined;
       }
-    var data = new Object();
     var structure = message.structure;
 
     // transform the column-major data set into column-major
-    // structured data
-    for (var sKey in structure) {
-      // should all be arrays
-      data[sKey]=_.object(
-        structure[sKey],
-        structure[sKey].map(function(sCol){
-          return message.table[sCol];
-        }))
-    }
-    return data
+    // structured data.
+    function tableMember(x){return message.table[x]};
+    data = 
+      structureWalker()
+        .other(tableMember)
+        (structure)
   }
   
-  // The I combinator
-  function identityF() { return function identity(x){return x} }
-  
-  // Apply a different function depending upon the type of the argument:
-  // Object/Array/Other.  Treats this as an argument to pass on.
-  // relies on caller invoking returned function using:
-  // applyObjectArrayOtherF.call(context,a,b,c) to load this properly
-  function applyObjectArrayOtherF(objF, arrayF, otherF) { 
-    return function applyObjectArrayOther(val) {
-    // map context is passed in as this
-    return ((_.isArray(val) ? arrayF: 
-           _.isObject(val) ? objF:
-            otherF).call(this, val, this));
-  }}
-  
-  // Apply a function to every property of an object and return
-  // the object constructed out of the results with the same keys
-  // pass a context object too.
-  function mapObjectF(f) { return function mapObject(obj, context) {
-      return _.object(_.keys(obj), _.map(_.values(obj), f, context));      
-  }}
-  
-  function mapArrayF(f) { return function mapArray(obj, context) {
-    return _.map(obj, f, context);
-  }}
-  
-  var mapValueF = identityF;
-  
-  function mapAnythingF(f) {
-    return applyObjectArrayOtherF(
-      mapObjectF(f),
-      mapArrayF(f),
-      mapValueF(f)
-      )
-  }
-  
-  // Something like the Y combinator for these functions
-  // allows recursion on subobjects.
-  // there's an issue with _.map since it passes the context
-  // argument via 'this'.  why does mapObjectF work?
-  function recurse(val,f) {
-    return f.call(f,val,f);
-  }  
-
-  
-  // The K combinator
-  function constantlyF(val) { return function constantly() { return val }}
   
   // Walk a structure composed of objects arrays and elements, and apply
   // one of the functions on each type.  Specify recurse to continue walking into
   // the object.
-  function structureWalker(objFn,arrayFn,otherFn) {
-    return mapAnythingF(
-      applyObjectArrayOtherF(
-        objFn,arrayFn,otherFn));
+  function structureWalker() {
+    // The I combinator
+    function identityF() { return function identity(x){return x} }
+    
+    // utility for consumers of structureWalker
+    identity = identityF();
+    
+    // Apply a different function depending upon the type of the argument:
+    // Object/Array/Other.  Treats this as an argument to pass on.
+    // relies on caller invoking returned function using:
+    // applyObjectArrayOtherF.call(context,a,b,c) to load this properly
+    function applyObjectArrayOtherF(objF, arrayF, otherF) { 
+      return function applyObjectArrayOther(val) {
+      // map context is passed in as this
+      return ((_.isArray(val) ? arrayF: 
+                 _.isObject(val) ? objF:
+                   otherF).call(this, val, this));
+    }}
+    
+    // Apply a function to every property of an object and return
+    // the object constructed out of the results with the same keys
+    // pass a context object too.
+    function mapObjectF(f) { return function mapObject(obj, context) {
+        return _.object(_.keys(obj), _.map(_.values(obj), f, context));      
+    }}
+    
+    function mapArrayF(f) { return function mapArray(obj, context) {
+      return _.map(obj, f, context);
+    }}
+    
+    var mapValueF = identityF;
+    
+    function mapAnythingF(f) {
+      return applyObjectArrayOtherF(
+        mapObjectF(f),
+        mapArrayF(f),
+        mapValueF(f)
+        )
+    }
+    
+    // Something like the Y combinator for these functions
+    // allows recursion on subobjects.  The default behaviour
+    // for arrays and objects.
+    function recurse(val,f) {
+      return f.call(f,val,f);
+    }  
+  
+    // The K combinator.  Probably want to export this.
+    function constantlyF(val) { return function constantly() { return val }}
+    
+    // generator for object member extractor
+    member = function member(anObject){return function(m){return anObject[m]}}
+    
+    var objectFn = recurse,
+        arrayFn = recurse,
+        otherFn = identity;
+    
+    function walk(structure) {
+        return recurse(structure,
+          mapAnythingF(
+            applyObjectArrayOtherF(
+              objectFn,arrayFn,otherFn)));
+    }
+    
+    walk.object = function(newObjectFn) {
+      objectFn = newObjectFn;
+      return walk;
+    }
+    
+    walk.array = function(newArrayFn) {
+      arrayFn = newArrayFn;
+      return walk;
+    }
+    
+    walk.other = function(newOtherFn) {
+      otherFn = newOtherFn;
+      return walk;
+    }
+    
+    return walk;
   }
   
   // Convert column major structure to row major
@@ -105,21 +129,7 @@
     // rowfiy?
     //plotData = aestheticData(lastData,message$aesthetic)
     
-    var T=function(x){return typeof(x)}
-
-    var Q=mapObjectF(applyObjectArrayOtherF(recurse,_.size,identityF()))
-    //Q(message,Q)
-    var Q2=mapAnythingF(applyObjectArrayOtherF(recurse,recurse,function(x){return ">"+x}));
-    var testStr = {a: [1,2,3], b: {a: "alex", b: "bob"}, c: "hello"};
     
-    // call recurse on one of these functions
-    recurse(message,Q)
-    recurse(message,Q2)
-    
-    T2=function(D){return mapObjectF(applyObjectArrayOtherF(recurse,recurse,D))}
-    T3=function(container){return T2(container,T2)}
-    M=function(C){return function(m){return C[m]}}
-    recurse(message.structure,T2(M(message.table)))
     
     var svg = d3.select(".d3io").select("svg")
 
